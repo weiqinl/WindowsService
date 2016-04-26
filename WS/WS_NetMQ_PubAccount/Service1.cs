c5b070510ad9443900db30fbc74c7ee23b6c06a0
liuwq <liuwq>
2016-04-26 11:06:38 +0800

    增加安装服务

--- WS/WS_NetMQ_PubAccount/Service1.cs
+++ WS/WS_NetMQ_PubAccount/Service1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.ServiceProcess;
 using System.Text;
@@ -20,17 +21,30 @@ namespace WS_NetMQ_PubAccount
         protected override void OnStart(string[] args)
         {
             string start = string.Format("{0}-{1}", DateTime.Now.ToString("yyyyMMddHHmmss"), "服务启动了");
+            Log(start);
 
         }
 
         protected override void OnStop()
         {
             string start = string.Format("{0}-{1}", DateTime.Now.ToString("yyyyMMddHHmmss"), "服务停止了");
+            Log(start);
         }
 
         protected override void OnShutdown()
         {
             string start = string.Format("{0}-{1}", DateTime.Now.ToString("yyyyMMddHHmmss"), "电脑关闭了");
+            Log(start);
+        }
+
+        void Log(string str)
+        {
+
+            string path = "E:6.txt";
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                sw.WriteLine(str);
+            }
         }
     }
 }
